ea802d8c2615211f30b780fb2f91003025b7e747
Reeya Bansal <73174396+reeya-24@users.noreply.github.com>
2023-04-11 13:45:16 +0530

    Create 2.First Missing Positive.java

---
+++ 6-Array Implementation/2.First Missing Positive.java
@@ -0,0 +1,47 @@
+//leetcode 41
+
+class Solution {
+    
+    public static int firstMissingPositive(int[] nums) {
+		
+		boolean flag = true;
+        for(int i = 0; i < nums.length; i++) {
+        	if(nums[i] == 1) {
+        		flag = false;
+        	}
+        }
+        if(flag) {
+        	return 1;
+        }
+        
+		// if(nums.length == 1) {
+		// 	if(nums[0] != 1) {
+		// 		return 1;
+		// 	} else {
+		// 		return 2;
+		// 	}
+		// }
+        
+        for(int i = 0; i < nums.length; i++) {
+        	if(nums[i] <= 0 || nums[i] > nums.length) {
+        		nums[i] = 1;
+        	}
+        }
+        
+       for(int i = 0; i < nums.length; i++) {
+    	   int val = Math.abs(nums[i]);
+    	   if(nums[val - 1] > 0) {
+    		   nums[val - 1] *= -1;
+    	   }
+       }
+       
+       for(int i = 0; i < nums.length; i++) {
+    	   if(nums[i] > 0) {
+    		   return i + 1;
+    	   }
+       }
+        
+        return nums.length+1;
+    }
+
+}
