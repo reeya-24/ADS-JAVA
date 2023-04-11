bacc70d4d27cb703f0cbfb15352d2fa16c83aa66
Reeya Bansal <73174396+reeya-24@users.noreply.github.com>
2023-04-11 19:37:22 +0530

    Create 1.Valid Parentheses.java

---
+++ 1.Valid Parentheses.java
@@ -0,0 +1,32 @@
+//leetcode 20
+class Solution {
+    public boolean isValid(String s) {
+        
+        Stack<Character> st = new Stack<>();
+        for(int i = 0 ; i < s.length(); i++)
+        {
+            char ch = s.charAt(i);
+            if(ch == '{')
+            {
+                st.push('}');
+            }
+            else if(ch == '(')
+            {
+                st.push(')');
+            }
+            else if(ch == '[')
+            {
+                st.push(']');
+            }
+            else
+            {
+                if(st.isEmpty() || st.peek() != ch)
+                    return false;
+                else
+                    st.pop();
+            }
+        }
+        return st.isEmpty();
+        
+    }
+}
