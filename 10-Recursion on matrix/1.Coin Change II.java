0817b27b1c4223a69fd5d1767850a185b1981c25
Reeya Bansal <73174396+reeya-24@users.noreply.github.com>
2023-04-11 19:30:48 +0530

    Create 1.Coin Change II.java

---
+++ 10-Recursion on matrix/1.Coin Change II.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int change(int amount, int[] coins) {
+        return changeCoin(amount,coins,0);
+    }
+    public int changeCoin(int amount, int[] coins, int k) {
+        if(amount==0){
+            return 1;
+        }
+        int ans=0;
+        for(int i=k; i<coins.length; i++){
+            if(amount>=coins[i])
+                ans+=changeCoin(amount-coins[i],coins,i);
+        }
+        return ans;
+        
+    }
+}
