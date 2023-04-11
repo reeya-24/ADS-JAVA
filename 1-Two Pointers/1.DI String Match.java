2fe847750fc7ac17827f41c4043992d610ca9154
Reeya Bansal <73174396+reeya-24@users.noreply.github.com>
2023-04-11 13:16:12 +0530

    Create 1.DI String Match.java

---
+++ 1-Two Pointers/1.DI String Match.java
@@ -0,0 +1,21 @@
+//leetcode 942
+
+class Solution {
+    public int[] diStringMatch(String s) {
+        int[] ans = new int[s.length()+1];
+        int start=0;
+        int end = s.length();
+        for(int i=0; i<s.length(); i++){
+            if(s.charAt(i)=='I'){
+                ans[i]=start;
+                start++;
+            }
+            else{
+                ans[i]=end;
+                end--;
+            }
+        }
+        ans[s.length()]=start;
+        return ans;
+    }
+}
