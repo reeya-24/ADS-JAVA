1df5b92ce2765562845b6e019a2bdc025c73ba75
Reeya Bansal <73174396+reeya-24@users.noreply.github.com>
2023-04-11 13:20:27 +0530

    Create 2.Count number of nice subarrays.java

---
+++ 2-Sliding Window/2.Count number of nice subarrays.java
@@ -0,0 +1,30 @@
+//leetcode 1248
+class Solution {
+    public int numberOfSubarrays(int[] nums, int k) {
+        
+        return atmostK(nums,k) - atmostK(nums,k-1);
+    }
+    public int atmostK(int[] nums,int k)
+    {
+        int start = 0;
+        int ans = 0;
+        for(int i = 0; i < nums.length; i++)
+        {
+            if((nums[i]&1) == 1)
+            {
+                k--;
+            }
+            while(k < 0)
+            {
+                if((nums[start]&1) == 1)
+                {
+                    k++;
+                }
+                start++;
+            }
+            ans += i-start+1;
+            
+        }
+        return ans;
+    }
+}
